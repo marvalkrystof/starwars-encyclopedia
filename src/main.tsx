b0a1a46071bfcdde6b0343de5e9f03e33f3245e4
marvalkrystof <marval.krystof@seznam.cz>
2024-12-09 19:42:18 +0100

    Import cleanup in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import theme from "./theme/Theme";
+import theme from "./theme/theme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
